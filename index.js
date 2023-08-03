a24d49545152a406d9e8be18eb09851e28fc43e7
Thomas Cort <thomasc@ssicanada.com>
2023-08-03 07:01:27 -0400

    feat: preliminary browser support

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 
 const AdifReader = require('./lib/AdifReader');
 const AdifWriter = require('./lib/AdifWriter');
+const Field = require('./lib/Field');
 const Header = require('./lib/Header');
 const QSO = require('./lib/QSO');
+const Version = require('./lib/Version');
 const defs = require('./lib/defs');
 const enums = require('./lib/enums');
 
 module.exports = {
     AdifReader, AdifWriter,
-    Header, QSO,
+    Field, Header, QSO, Version,
     defs,
     enums,
 };
