f32101258f53c6e70594aabf8653a522cfa83221
Thomas Cort <linuxgeek@gmail.com>
2023-10-26 02:56:08 -0400

    chore: bump browser version

--- dist/tcadif.js
+++ dist/tcadif.js
@@ -16480,7 +16480,7 @@ function config (name) {
 },{}],210:[function(require,module,exports){
 module.exports={
   "name": "tcadif",
-  "version": "1.5.0",
+  "version": "1.5.1",
   "description": "read and write Amateur Data Interchange Format (ADIF)",
   "main": "index.js",
   "scripts": {
