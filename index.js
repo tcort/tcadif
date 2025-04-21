a45edabea5d9ab63e9f3e61b4ce8f832cfae6445
Thomas Cort <linuxgeek@gmail.com>
2025-04-21 07:30:41 -0400

    feat(index): export enums

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const AdifWriter = require('./lib/AdifWriter');
 const Header = require('./lib/Header');
 const QSO = require('./lib/QSO');
 const defs = require('./lib/defs');
+const enums = require('./lib/enums');
 
 module.exports = {
     AdifReader, AdifWriter,
     Header, QSO,
     defs,
+    enums,
 };
