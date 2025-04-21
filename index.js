d7e53a541e6a3d8982b14c1bb1cc00670c302326
Thomas Cort <linuxgeek@gmail.com>
2025-04-21 07:30:41 -0400

    feat: expose field definitions

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const AdifReader = require('./lib/AdifReader');
 const AdifWriter = require('./lib/AdifWriter');
 const Header = require('./lib/Header');
 const QSO = require('./lib/QSO');
+const defs = require('./lib/defs');
 
 module.exports = {
     AdifReader, AdifWriter,
     Header, QSO,
+    defs,
 };
