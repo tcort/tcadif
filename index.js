d5697ee7a866318b042e5ca51120a28a68e3f9c8
Thomas Cort <linuxgeek@gmail.com>
2025-04-21 07:30:42 -0400

    refactor: drop AdifReader/AdifWriter interfaces, too buggy

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const AdifReader = require('./lib/AdifReader');
-const AdifWriter = require('./lib/AdifWriter');
 const Field = require('./lib/Field');
 const Header = require('./lib/Header');
 const QSO = require('./lib/QSO');
@@ -11,7 +9,6 @@ const defs = require('./lib/defs');
 const enums = require('./lib/enums');
 
 module.exports = {
-    AdifReader, AdifWriter,
     ADIF, Field, Header, QSO, Version,
     defs,
     enums,
