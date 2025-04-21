3b5a04eec821cddeb39e29cf81acb421f44ebbd7
Thomas Cort <linuxgeek@gmail.com>
2025-04-21 07:30:43 -0400

    fix(enums): make case insensitive

--- test/QSO.test.js
+++ test/QSO.test.js
@@ -14,9 +14,9 @@ describe('QSO', function () {
                 CALL: 'W1AW',
                 TIME_ON: '1234',
                 QSO_DATE: '20191211',
-                BAND: '40m',
+                BAND: '40M',
                 MODE: 'CW',
-            }).stringify()).to.be('<BAND:3:E>40m\n<CALL:4:S>W1AW\n<MODE:2:E>CW\n<QSO_DATE:8:D>20191211\n<TIME_ON:4:T>1234\n<EOR>');
+            }).stringify()).to.be('<BAND:3:E>40M\n<CALL:4:S>W1AW\n<MODE:2:E>CW\n<QSO_DATE:8:D>20191211\n<TIME_ON:4:T>1234\n<EOR>');
         });
     });
 });
