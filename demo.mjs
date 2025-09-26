f64ab3bdc281092a027965555f9bb1b03b60ccd4
Thomas Cort <linuxgeek@gmail.com>
2025-09-26 19:05:35 -0400

    feat: implement new fields

---
+++ demo.mjs
@@ -0,0 +1,30 @@
+'use strict';
+
+import File from './lib/files/File.mjs';
+import HeaderSegment from './lib/segments/HeaderSegment.mjs';
+
+const f = File.fromJSON({
+    HEADER: HeaderSegment.factory(),
+    RECORDS: [
+        {
+            CALL: 'VA2NW',
+            QSO_DATE: '20250925',
+            QSO_DATE_OFF: '20250925',
+            BAND: '20m',
+            ITUZ: 4,
+            TIME_ON: '2133',
+            TIME_OFF: '213344',
+            K_INDEX: '04',
+            MODE: 'SSB',
+            ADDRESS: 'ATTN: Hambone\r\n123 Main St',
+            AGE: '43',
+            ANT_AZ: '370',
+            ANT_EL: 100,
+            ANT_PATH: 'L',
+            ARRL_SECT: 'VT',
+            AWARD_SUBMITTED: 'ARRL_WAS,ADIF_UTF',
+        },
+    ]
+});
+console.log(f.toADI());
+console.log(f.toJSON());
